f431df285560637948b0c9a2f9272ecfd5901e8f
Tom Liang <tomliangg@gmail.com>
2018-12-11 23:28:31 -0800

    added redux

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { Fragment, Component } from 'react'
+import Counter from './components/Counter'
+import CounterReducer from './reducers/CounterReducer'
+import { createStore } from 'redux'
+
+const store = createStore(CounterReducer)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <Counter
+          initialValue = {store.getState()}
+          HandleClick={() => store.dispatch({ type: 'INCREMENT' })}
+        />
+      </Fragment>
     )
   }
 }
+
+
+store.subscribe(() => {
+  console.log('state updated')
+  console.log(store.getState())
+})
 
 export default App
