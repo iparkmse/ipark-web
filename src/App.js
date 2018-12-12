4e067194c87151a028eb439ec6bfed5fffdd5518
Tom Liang <tomliangg@gmail.com>
2018-12-11 23:53:53 -0800

    added firebase, styled-components, and router; removed redux

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React, { Fragment, Component } from 'react'
-import Counter from './components/Counter'
-import CounterReducer from './reducers/CounterReducer'
-import { createStore } from 'redux'
-
-const store = createStore(CounterReducer)
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Counter
-          initialValue = {store.getState()}
-          HandleClick={() => store.dispatch({ type: 'INCREMENT' })}
-        />
+        <h1>Bare Minimal</h1>
       </Fragment>
     )
   }
 }
-
-
-store.subscribe(() => {
-  console.log('state updated')
-  console.log(store.getState())
-})
 
 export default App
