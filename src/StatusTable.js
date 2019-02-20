1229434882a23519697e635d2e2395e9e6bb3df8
Ramita Tr <loogmai@Loogmais-MacBook-Pro.local>
2019-02-19 23:43:38 -0800

    Fixed the border display issue

--- src/StatusTable.js
+++ src/StatusTable.js
@@ -1,28 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const Table = styled.table`
+  border: 1px solid black;
+  border-collapse: collapse;
+`
+
+const TableR = styled.tr`
+  border: 1px solid black;
+  border-collapse: collapse;
+`
+const TableD = styled.td`
+  border: 1px solid black;
+  border-collapse: collapse;
+`
 
 const StatusTable = ({data}) => {
   const ind = Array.from(Array(data.length/2).keys())
   return(
-    <table>
+    <Table>
       <thead>
-        <tr>
+        <TableR>
           <th colSpan='4'>PARKING STATUS</th>
-        </tr>
+        </TableR>
       </thead>
       <tbody>
         {
           ind.map(i => {
             return(
-              <tr key={data[i].id}>
-                <td>{data[i].ID}  {data[i].status}</td>
-                <td>{data[i+5].ID}  {data[i+5].status}</td>
-              </tr>
+              <TableR key={data[i].id}>
+                <TableD>{data[i].ID}  {data[i].status}</TableD>
+                <TableD>{data[i+5].ID}  {data[i+5].status}</TableD>
+              </TableR>
             )
           })
         }
       </tbody>
-    </table>
+    </Table>
   )
 }
 
