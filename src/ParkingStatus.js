ca3504ebe100a6582694a38d5ad7b3ca163c3610
Ramita Tr <loogmai@Loogmais-MacBook-Pro.local>
2019-02-21 12:00:29 -0800

    Styled status table + added status summary and legend components

--- src/ParkingStatus.js
+++ src/ParkingStatus.js
@@ -1,24 +1,63 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import StatusTable from './StatusTable'
+import StatusSummary from './StatusSummary'
+import StatusLegend from './StatusLegend'
+
+const Wrapper = styled.div`
+  background-color: rgba(0, 0, 0, 0.6);  // Black with Transparency of 40%
+  width: 900px;
+  padding: 60px 10px;
+`
+
+const Table = styled.table`
+  margin: auto;
+`
+
+const Row = styled.tr`
+  vertical-align: top;
+`
+
+const SummaryCell = styled.td`
+  padding: 1px 50px 1px 20px;
+`
+
+const StatusCell = styled.td`
+  padding: 1px 50px 1px 50px;
+`
+
+const LegendCell = styled.td`
+  padding: 1px 20px 1px 50px;
+`
 
 class ParkingStatus extends Component {
   state = {
     stall: [
-      {ID: 'A1', status: 'Vacant', id: 1},
-      {ID: 'A2', status: 'Ocupied', id: 2},
-      {ID: 'A3', status: 'Vacant', id: 3},
-      {ID: 'A4', status: 'Ocupied', id: 4},
-      {ID: 'A5', status: 'Vacant', id: 5},
-      {ID: 'B1', status: 'Vacant', id: 6},
-      {ID: 'B2', status: 'Vacant', id: 7},
-      {ID: 'B3', status: 'Vacant', id: 8},
-      {ID: 'B4', status: 'Ocupied', id: 9},
-      {ID: 'B5', status: 'Ocupied', id: 10},
-    ]
+      {ID: 'A1', status: 'vacant', index: 0},
+      {ID: 'A2', status: 'ocupied', index: 1},
+      {ID: 'A3', status: 'vacant', index: 2},
+      {ID: 'A4', status: 'ocupied', index: 3},
+      {ID: 'A5', status: 'vacant', index: 4},
+      {ID: 'B1', status: 'vacant', index: 5},
+      {ID: 'B2', status: 'vacant', index: 6},
+      {ID: 'B3', status: 'vacant', index: 7},
+      {ID: 'B4', status: 'ocupied', index: 8},
+      {ID: 'B5', status: 'reserved', index: 9},
+    ],
   }
   render(){
     return(
-      <StatusTable data={this.state.stall}/>
+      <Wrapper>
+        <Table>
+          <tbody>
+            <Row>
+              <SummaryCell><StatusSummary data={this.state.stall}/></SummaryCell>
+              <StatusCell><StatusTable data={this.state.stall}/></StatusCell>
+              <LegendCell><StatusLegend/></LegendCell>
+            </Row>
+          </tbody>
+        </Table>
+      </Wrapper>
     )
   }
 }
--- src/StatusTable.js
+++ src/StatusTable.js
@@ -1,19 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import StallStatus from './StallStatus'
 
 const Table = styled.table`
-  border: 1px solid black;
   border-collapse: collapse;
+  text-align: center;
+  font-family: 'Gill Sans', sans-serif;
+  /* margin-left: auto;
+  margin-right: auto; */
 `
 
-const TableR = styled.tr`
-  border: 1px solid black;
-  border-collapse: collapse;
+const LeftColumn = styled.td`
+  border-left: none;
+  border-right: 5px solid whitesmoke;
+  border-bottom: 5px solid whitesmoke;
+  border-top: 5px solid whitesmoke;
+  padding: 10px 30px 10px 20px;
 `
-const TableD = styled.td`
-  border: 1px solid black;
-  border-collapse: collapse;
+
+const RightColumn = styled(LeftColumn)`
+  border-left: 5px solid whitesmoke;
+  border-right: none;
+  padding: 10px 20px 10px 30px;
+`
+
+const LabelTitle = styled.label`
+  color: NavajoWhite;
+  font-size: 28px;
+  letter-spacing: 1px;
+`
+
+const LabelStallID = styled.label`
+  color: NavajoWhite;
+  font-size: 28px;
+  padding: 10px 20px 10px 20px;
+  vertical-align: middle;
+  letter-spacing: 1px;
 `
 
 const StatusTable = ({data}) => {
@@ -21,18 +44,26 @@ const StatusTable = ({data}) => {
   return(
     <Table>
       <thead>
-        <TableR>
-          <th colSpan='4'>PARKING STATUS</th>
-        </TableR>
+        <tr>
+          <th colSpan='4'>
+            <LabelTitle>PARKING STATUS</LabelTitle>
+          </th>
+        </tr>
       </thead>
       <tbody>
         {
           ind.map(i => {
             return(
-              <TableR key={data[i].id}>
-                <TableD>{data[i].ID}  {data[i].status}</TableD>
-                <TableD>{data[i+5].ID}  {data[i+5].status}</TableD>
-              </TableR>
+              <tr key={data[i].index}>
+                <LeftColumn>
+                  <LabelStallID>{data[i].ID}</LabelStallID>
+                  <StallStatus status={data[i].status}/>
+                </LeftColumn>
+                <RightColumn>
+                  <StallStatus status={data[i+5].status}/>
+                  <LabelStallID>{data[i+5].ID}</LabelStallID>
+                </RightColumn>
+              </tr>
             )
           })
         }
