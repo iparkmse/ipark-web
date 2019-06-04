5a5540e4fd2d01fe577050fbe65d955e1c94d101
Ramita Tr <loogmai39@gmail.com>
2019-06-03 22:22:25 -0700

    inputs validation for signup form

--- src/components/userAuth/SignupForm.js
+++ src/components/userAuth/SignupForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import { Button, Form, Input, Title } from './LoginForm'
+import firebaseApp from '../../firebase'
 
 const GridWrapper = styled.div`
   display: grid;
@@ -18,33 +19,63 @@ const NameGrid = styled.div`
   column-gap: 10px;
 `
 
+const validate = (email, pass, confirmPass, fName, lName, licensePlate) => {
+  const rgEmail = /\S+@\S+\.\S+/
+  const rgName = /^[a-z ,.'-]+$/i
+  return {
+    email: !rgEmail.test(email),
+    pass: pass.length < 8,
+    confirmPass: confirmPass !== pass,
+    fName: !rgName.test(fName),
+    lName: !rgName.test(lName),
+    licensePlate: licensePlate.length < 4
+  }
+}
+
 export default class SignupForm extends Component {
   state = {
-    email: null,
-    pass: null,
-    confirmPass: null,
-    fName: null,
-    lName: null,
-    licensePlate: null
+    email: '',
+    pass: '',
+    confirmPass: '',
+    fName: '',
+    lName: '',
+    licensePlate: ''
+  }
+
+  canBeSubmitted = () => {
+    const errors = validate(this.state.email, this.state.pass, this.state.confirmPass, this.state.fName, this.state.lName, this.state.licensePlate)
+    const isDisabled = Object.keys(errors).some(i => errors[i])
+    return !isDisabled
   }
 
   handleSubmit = (e) => {
+    if (!this.canBeSubmitted()) {
+      e.preventDefault()
+      console.log('some fields are invalid')
+      return
+    }
     e.preventDefault()
+    console.log(this.state.email)
   }
-
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
   render() {
+    const errors = validate(this.state.email, this.state.pass, this.state.confirmPass, this.state.fName, this.state.lName, this.state.licensePlate)
+    const isDisabled = Object.keys(errors).some(i => errors[i])
+
     return (
       <Form onSubmit={this.handleSubmit}>
         <Title>WELCOME TO IPARK</Title>
         <GridWrapper>
           <NameGrid>
-            <Input type='text' name='fname' placeholder='first name' />
-            <Input type='text' name='lname' placeholder='last name' />
+            <Input type='text' name='fName' placeholder='first name' onChange={this.handleChange} required />
+            <Input type='text' name='lName' placeholder='last name' onChange={this.handleChange} required />
           </NameGrid>
-          <Input type='email' name='email' placeholder='email' />
-          <Input type='password' name='pass' placeholder='password' />
-          <Input type='password' name='cpass' placeholder='confirm password' />
-          <Input type='text' name='license' placeholder='license plate' />
+          <Input type='email' name='email' placeholder='email' onChange={this.handleChange} required />
+          <Input type='password' name='pass' placeholder='password' onChange={this.handleChange} required />
+          <Input type='password' name='confirmPass' placeholder='confirm password' onChange={this.handleChange} required />
+          <Input type='text' name='licensePlate' placeholder='license plate' onChange={this.handleChange} required />
         </GridWrapper>
         <Button type='submit'>Sign Up</Button>
       </Form>
