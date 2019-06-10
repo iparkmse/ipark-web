68df8dda43cbfe77837f96503d9a6e5750597376
Tom Liang <tomliangg@gmail.com>
2019-06-10 11:03:53 -0700

    style updates for ResCell when mouse hover

--- src/components/reservation/ResCell.js
+++ src/components/reservation/ResCell.js
@@ -6,18 +6,30 @@ const BookedCell = styled.div`
   background-color: papayawhip;
   border: 1px black solid;
   border-left: none;
+  color: silver;
+  font-style: italic;
+
+  :hover {
+    cursor: not-allowed;
+  }
 `
 
 const FreeCell = styled.div`
   background-color: white;
   border: 1px black solid;
   border-left: none;
+
+  :hover {
+    background-color: paleturquoise;
+  }
 `
 
 
 const ResCell = ({ uid }) => {
   return (uid ? (
-    <BookedCell />
+    <BookedCell>
+      [booked]
+    </BookedCell>
   ) : (
     <FreeCell />
   ))
