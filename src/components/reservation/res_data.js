5c6965bfb374c7f2e08e2cbd5b32a7f072b81dfc
Tom Liang <tomliangg@gmail.com>
2019-06-16 21:12:17 -0700

    add one more field/column (namely validated) to res table; use random str as bookingID

--- src/components/reservation/res_data.js
+++ src/components/reservation/res_data.js
@@ -1,226 +1,57 @@
+const idGen = () => (
+  Math.random().toString(36).substring(2, 15)
+  + Math.random().toString(36).substring(2, 15)
+)  // generate random str consisted of num & letters for bookingID
+
+const stallA1 = {
+  a7: {index: 0, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  b8: {index: 1, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  c9: {index: 2, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  d10: {index: 3, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  e11: {index: 4, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  f12: {index: 5, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  g13: {index: 6, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  h14: {index: 7, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  i15: {index: 8, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  j16: {index: 9, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  k17: {index: 10, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  l18: {index: 11, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+}
+
+const stallA2 = {
+  a7: {index: 12, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  b8: {index: 13, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  c9: {index: 14, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  d10: {index: 15, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  e11: {index: 16, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  f12: {index: 17, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  g13: {index: 18, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  h14: {index: 19, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  i15: {index: 20, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  j16: {index: 21, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  k17: {index: 22, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  l18: {index: 23, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+}
+
+const stallA3 = {
+  a7: {index: 24, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  b8: {index: 25, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  c9: {index: 26, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  d10: {index: 27, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  e11: {index: 28, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  f12: {index: 29, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  g13: {index: 30, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  h14: {index: 31, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  i15: {index: 32, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  j16: {index: 33, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  k17: {index: 34, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+  l18: {index: 35, bookingID: idGen(), uid: '', bookingRef: '', validated: false},
+}
+
 const RES_DATA = {
-  'stallA1': {
-    'a7': {
-      'index': 0,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'b8': {
-      'index': 1,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'c9': {
-      'index': 2,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'd10': {
-      'index': 3,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'e11': {
-      'index': 4,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'f12': {
-      'index': 5,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'g13': {
-      'index': 6,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'h14': {
-      'index': 7,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'i15': {
-      'index': 8,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'j16': {
-      'index': 9,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'k17': {
-      'index': 10,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'l18': {
-      'index': 11,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    }
-  },
-  'stallA2': {
-    'a7': {
-      'index': 12,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'b8': {
-      'index': 13,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'c9': {
-      'index': 14,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'd10': {
-      'index': 15,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'e11': {
-      'index': 16,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'f12': {
-      'index': 17,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'g13': {
-      'index': 18,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'h14': {
-      'index': 19,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'i15': {
-      'index': 20,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'j16': {
-      'index': 21,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'k17': {
-      'index': 22,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'l18': {
-      'index': 23,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    }
-  },
-  'stallA3': {
-    'a7': {
-      'index': 24,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'b8': {
-      'index': 25,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'c9': {
-      'index': 26,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'd10': {
-      'index': 27,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'e11': {
-      'index': 28,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'f12': {
-      'index': 29,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'g13': {
-      'index': 30,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'h14': {
-      'index': 31,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'i15': {
-      'index': 32,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'j16': {
-      'index': 33,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'k17': {
-      'index': 34,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    },
-    'l18': {
-      'index': 35,
-      'bookingID': '',
-      'uid': '',
-      'bookingRef': ''
-    }
-  }
+  stallA1: stallA1,
+  stallA2: stallA2,
+  stallA3: stallA3
 }
 
 export default RES_DATA
