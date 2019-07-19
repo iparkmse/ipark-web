cc7bd6c25772c38f358c09892a9d37f4dda27f7c
Tom Liang <tomliangg@gmail.com>
2019-07-18 21:39:51 -0700

    refactor ResVal: coding style

--- src/components/reservation/ResVal.js
+++ src/components/reservation/ResVal.js
@@ -4,7 +4,10 @@ import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import Modal from '@material-ui/core/Modal'
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
+import { LinkWrapper, ResWrapper, BtnWrapper } from './constants'
+import { ResValLink, ResTodayLink } from './ResLink'
 import { modalStyle } from './ResModal'
+
 
 const headerStyle = {
   margin: '0px 0px 20px 0px',
@@ -12,16 +15,8 @@ const headerStyle = {
   fontSize: '25px'
 }
 
-const bottonStyleVal = {
+const btnStyle = {
   margin: '20px 0px 0px 0px',
-  color: 'green',
-  fontSize: '18px',
-  fontWeight: 'bold'
-}
-
-const bottonStyleCan = {
-  margin: '20px 0px 0px 0px',
-  color: 'grey',
   fontSize: '18px',
   fontWeight: 'bold'
 }
@@ -77,53 +72,61 @@ class ResVal extends Component {
   render() {
     const { open, error, input } = this.state
     return(
-      <MuiThemeProvider theme={theme}>
-        <Typography gutterBottom>Placeholder for booking validation button</Typography>
-        <Button onClick={this.handleOpen}>Booking Validation</Button>
-        <Modal
-          aria-labelledby='reservation-validation-modal'
-          open={open}
-          onClose={this.handleClose}
-        >
-          <div style={{...modalStyle, textAlign: 'center'}}>
-            <Typography variant='h6' style={headerStyle}>
-              BOOKING VALIDATION
-            </Typography>
-            <TextField
-              label='Booking Reference'
-              autoFocus
-              error={error}
-              InputProps={{
-                style: { fontSize: '22px' }
-              }}
-              inputProps={{
-                style: { textAlign: 'center', letterSpacing: '10px' },
-                maxLength: 4
-              }}
-              variant='outlined'
-              value={input}
-              onChange={this.handleChange}
-            />
-            {error && (
-              <Typography variant="body2" style={bodyStyle}>
-                The booking reference does not match our record
-                <br/>
-                Please re-enter the correct booking reference
+      <ResWrapper>
+        <LinkWrapper>
+          <ResValLink />
+          <ResTodayLink />
+        </LinkWrapper>
+        <MuiThemeProvider theme={theme}>
+          <BtnWrapper>
+            <Button style={{ ...btnStyle, fontSize: '16px' }} onClick={this.handleOpen}>
+              Validate my booking
+            </Button>
+          </BtnWrapper>
+          <Modal
+            aria-labelledby='reservation-validation-modal'
+            open={open}
+            onClose={this.handleClose}
+          >
+            <div style={{...modalStyle, textAlign: 'center'}}>
+              <Typography variant='h6' style={headerStyle}>
+                BOOKING VALIDATION
               </Typography>
-            )}
-            <br/>
-            <Button style={bottonStyleVal} onClick={this.handleClick}>
-              VALIDATE
-            </Button>
-            <Button style={bottonStyleCan} onClick={this.handleClose}>
-              CANCEL
-            </Button>
-          </div>
-        </Modal>
-      </MuiThemeProvider>
+              <TextField
+                label='Booking Reference'
+                autoFocus
+                error={error}
+                InputProps={{
+                  style: { fontSize: '22px' }
+                }}
+                inputProps={{
+                  style: { textAlign: 'center', letterSpacing: '10px' },
+                  maxLength: 4
+                }}
+                variant='outlined'
+                value={input}
+                onChange={this.handleChange}
+              />
+              {error && (
+                <Typography variant="body2" style={bodyStyle}>
+                  The booking reference does not match our record
+                  <br/>
+                  Please re-enter the correct booking reference
+                </Typography>
+              )}
+              <br/>
+              <Button style={{ ...btnStyle, color: 'green' }} onClick={this.handleClick}>
+                VALIDATE
+              </Button>
+              <Button style={{ ...btnStyle, color: 'grey' }} onClick={this.handleClose}>
+                CANCEL
+              </Button>
+            </div>
+          </Modal>
+        </MuiThemeProvider>
+      </ResWrapper>
     )
   }
 }
 
 export default ResVal
-
